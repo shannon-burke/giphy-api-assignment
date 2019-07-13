cf38d33488e8781cd40f40fa79385f604fe26459
Shannon <shannonburke.art@gmail.com>
2019-07-13 16:50:04 -0500

    correcting my work flow

---
+++ logic.js
@@ -0,0 +1,16 @@
+//empty array for buttons
+buttonsArray = [];
+
+
+//on click function to push the user input to buttonsArray
+$("#giphy-submit-button").on("click", function (event) {
+    event.preventDefault();
+    var newButton = $("#button-input").val().trim();
+    buttonsArray.push(newButton);
+    makeButtons();
+});
+
+//this is the script to generate buttons for the user to click
+function makeButtons() {
+
+}
