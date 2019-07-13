d0b41e42f9b311281225e50148f9c759916ca6b3
Shannon <shannonburke.art@gmail.com>
2019-07-13 17:14:50 -0500

    button generation working

--- logic.js
+++ logic.js
@@ -2,6 +2,19 @@
 buttonsArray = [];
 
 
+//this is the script to generate buttons for the user to click
+function makeButtons() {
+    $("#buttons-div").empty();
+
+    for (var i = 0; i < buttonsArray.length; i++) {
+        var b = $("<button>");
+        b.addClass("gif-button btn btn-secondary");
+        b.attr("data-name", buttonsArray[i]);
+        b.text(buttonsArray[i]);
+        $("#buttons-div").append(b);
+    }
+}
+
 
 //on click function to push the user input to buttonsArray
 $("#giphy-submit-button").on("click", function (event) {
@@ -11,7 +24,4 @@ $("#giphy-submit-button").on("click", function (event) {
     makeButtons();
 });
 
-//this is the script to generate buttons for the user to click
-function makeButtons() {
 
-}
